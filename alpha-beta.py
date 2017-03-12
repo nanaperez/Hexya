479385d51fd59f7106c8beaadf96d7d14e237655
osoyei <yeison-arley25@hotmail.com>
2017-03-12 18:17:32 +0000

    creo que falta función de evaluación y retornar respuesta

--- alpha-beta.py
+++ alpha-beta.py
@@ -2,18 +2,20 @@ import numpy as np
 import random
 
 infinity = float('inf')
-       
+
+
 def getMoves(estado):
     libres = []
     for i in range(len(estado)):
         for j in range(len(estado)):
-          if ( estado[i][j] == 0 ):
-            libres.append((i,j))
+            if (estado[i][j] == 0):
+                libres.append((i, j))
     return libres
-    
+
+
 def evaluate(estado):
-    heuristic = random.randint(0, 11)
-    return heuristic
+    function = random.randint(0, 11)
+    return function
 
 
 def alphabeta(state, depth, alpha, beta, player):
@@ -25,8 +27,8 @@ def alphabeta(state, depth, alpha, beta, player):
         v = -infinity
         newState = state
         for child in moves:
-            newState[child] = 2
-            v = max(v, alphabeta(newState, depth -1, alpha, beta, 2))
+            newState[child] = 1
+            v = max(v, alphabeta(newState, depth - 1, alpha, beta, 2))
             alpha = max(alpha, v)
             if (beta <= alpha):
                 break
@@ -36,18 +38,18 @@ def alphabeta(state, depth, alpha, beta, player):
         v = infinity
         newState = state
         for child in moves:
-            newState[child] = 1
-            v = min(v, alphabeta(newState, depth -1, alpha, beta, 1))
+            newState[child] = 2
+            v = min(v, alphabeta(newState, depth - 1, alpha, beta, 1))
             beta = min(beta, v)
             if beta <= alpha:
                 break
         print newState
         return v
 
-def agente(state, player):
-    return alphabeta(state, 3, -infinity, infinity, player)
-    
 
-    
-tablero = np.zeros((8, 8))
+def agente(state, player):
+    return alphabeta(state, 100, -infinity, infinity, player)
+
+
+tablero = np.zeros((11, 11))
 print agente(tablero, 1)
